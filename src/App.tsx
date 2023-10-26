446ee61468f58c9586fab1a07d1f3c0eb1c86da6
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2023-10-26 09:57:16 +0200

    register component finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { useState } from 'react'
 import './App.css'
+import { LoginContext } from './components/login/context/authcontext'
 import { Register } from './components/login/register'
 
 function App() {
   
+  const [token, setToken] = useState<string>('')
+
   return (
+    <>
+    <LoginContext.Provider value={{token, setToken}}>
     <Register/>
+    </LoginContext.Provider>
+    </>
     )
   }
 
