b2a344ab08b3f0f97372d9b312a43c19f9ff4645
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2023-11-01 13:53:17 +0100

    updated app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
-import { useState } from 'react'
-import './App.css'
-import { LoginContext } from './context/authcontext'
-import { Register } from './components/login/Register'
-import { Login } from './components/login/Login'
-
+import { useState } from 'react';
+import './App.css';
+import { LoginContext } from './context/authcontext';
+import { Register } from './components/login/register';
+import { Login } from './components/login/login';
+import {Routes, Route} from 'react-router-dom';
+import { Homepage } from './components/homepage/Homepage';
 function App() {
   
   const [token, setToken] = useState<string>('')
 
-  return (
-    <>
+  if (token === '') return <>
+  <LoginContext.Provider value={{token, setToken}}>
+  <Register/>
+  <Login/>
+  </LoginContext.Provider>
+  </>
+
+  return <>
     <LoginContext.Provider value={{token, setToken}}>
-    <Register/>
-    <Login/>
+    <Homepage/>
     </LoginContext.Provider>
     </>
-    )
+    
   }
 
 export default App
